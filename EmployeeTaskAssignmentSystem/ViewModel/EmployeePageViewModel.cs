35fb5b548d51dd78709f98346e7f50c42a419efb
Bibek Parajuli <parajulibbek11@gmail.com>
2023-09-05 15:19:42 +0545

    design: create employee page design

---
+++ EmployeeTaskAssignmentSystem/ViewModel/EmployeePageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeTaskAssignmentSystem.Command;
+using EmployeeTaskAssignmentSystem.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EmployeeTaskAssignmentSystem.ViewModel
+{
+    public class EmployeePageViewModel: ViewModelBase
+    {
+        private ICommand _openEmployeeModalCommand;
+        public ICommand OpenEmployeeModelCommand
+        {
+            get
+            {
+                if (_openEmployeeModalCommand == null)
+                {
+                    _openEmployeeModalCommand = new RelayCommand(OpenEmployeeModal);
+                }
+                return _openEmployeeModalCommand;
+            }
+        }
+        private void OpenEmployeeModal()
+        {
+            ModalEmployee modalView = new ModalEmployee();
+            modalView.ShowDialog();
+        }
+    }
+}
