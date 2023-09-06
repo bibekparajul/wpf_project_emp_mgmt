77620eab73a2e05313aba417b09cd9ecf7bcda91
Bibek Parajuli <parajulibbek11@gmail.com>
2023-09-06 14:20:48 +0545

    design: home page design and dashboard setup

--- EmployeeTaskAssignmentSystem/ViewModel/HomePageViewModel.cs
+++ EmployeeTaskAssignmentSystem/ViewModel/HomePageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeTaskAssignmentSystem.Data;
+using EmployeeTaskAssignmentSystem.Model;
 using System.Linq;
 
 namespace EmployeeTaskAssignmentSystem.ViewModel
@@ -14,6 +15,10 @@ namespace EmployeeTaskAssignmentSystem.ViewModel
             // Calculate the total counts from the database
             TotalEmployees = appDbContext.Employees.Count();
             TotalTasks = appDbContext.Tasks.Count();
+            PendingTasks = appDbContext.Tasks.Count(t => t.Status == TaskStatus.Pending);
+            InProgressTasks = appDbContext.Tasks.Count(t => t.Status == TaskStatus.InProgress);
+            DoneTasks = appDbContext.Tasks.Count(t => t.Status == TaskStatus.Done);
+            NotStartedTasks = appDbContext.Tasks.Count(t => t.Status == TaskStatus.NotStarted);
         }
 
         private int _totalEmployees;
@@ -40,6 +45,61 @@ namespace EmployeeTaskAssignmentSystem.ViewModel
                 {
                     _totalTasks = value;
                     OnPropertyChanged(nameof(TotalTasks));
+                }
+            }
+        }
+        private int _pendingTasks;
+        public int PendingTasks
+        {
+            get => _pendingTasks;
+            set
+            {
+                if (_pendingTasks != value)
+                {
+                    _pendingTasks = value;
+                    OnPropertyChanged(nameof(PendingTasks));
+                }
+            }
+        }
+
+        private int _inProgressTasks;
+        public int InProgressTasks
+        {
+            get => _inProgressTasks;
+            set
+            {
+                if (_inProgressTasks != value)
+                {
+                    _inProgressTasks = value;
+                    OnPropertyChanged(nameof(InProgressTasks));
+                }
+            }
+        }
+
+        private int _doneTasks;
+        public int DoneTasks
+        {
+            get => _doneTasks;
+            set
+            {
+                if (_doneTasks != value)
+                {
+                    _doneTasks = value;
+                    OnPropertyChanged(nameof(DoneTasks));
+                }
+            }
+        }
+
+        private int _notStartedTasks;
+        public int NotStartedTasks
+        {
+            get => _notStartedTasks;
+            set
+            {
+                if (_notStartedTasks != value)
+                {
+                    _notStartedTasks = value;
+                    OnPropertyChanged(nameof(NotStartedTasks));
                 }
             }
         }
